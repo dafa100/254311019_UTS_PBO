98fb8f6da2e4aa0c5f483930e7f9412a7ffc2c14
dafa100 <dafasebastian017@gmail.com>
2026-04-14 14:47:19 +0700

    modified:   Tes.java

--- Tes.java
+++ Tes.java
@@ -1,3 +1,51 @@
-public class Tes{
+abstract class LayananTransportasi {
+    protected String namaDriver;
+    protected double jarak;
+    protected double saldoUser;
 
+    public LayananTransportasi(String namaDriver, double jarak, double saldoUser){
+        this.namaDriver = namaDriver;
+        this.saldoUser = saldoUser;
+        if (jarak <= 0) {
+            this.jarak = 1;
+        } else {
+            this.jarak = jarak;
+        }
+    }
+    public abstract void hitungTarif();
+}
+
+class MadiunMotor extends LayananTransportasi {
+    private double tarif;
+    private double nim;
+    private String nama;
+    
+    public MadiunMotor(String namaDriver, double jarak, double saldoUser, double nim, String nama){
+        super(namaDriver, jarak, saldoUser);
+        this.tarif = 2500;
+        this.nim = nim;
+        this.nama = nama;
+    }
+
+    @Override
+    public void hitungTarif(){
+        System.out.println("Tarif perjalanan adalah: "+tarif+" / km");
+    }
+    
+    public void prosesTransaksi(){
+        System.out.println("Transaksi oleh ");
+    }
+}
+
+class MadiunMobil extends LayananTransportasi {
+    private double tarif = 5000;
+    
+    public MadiunMobil(String namaDriver, double jarak, double saldoUser){
+        super(namaDriver, jarak, saldoUser);
+    }
+
+    @Override
+    public void hitungTarif(){
+        System.out.println("Tarif perjalanan adalah: "+tarif+" / km");
+    }
 }
